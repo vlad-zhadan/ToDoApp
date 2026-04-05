456522b5bccadd87ca4b6a6fe87369512d6fe233
vlad-zhadan <zhadan.vld@gmail.com>
2026-04-05 15:01:14 +0300

    add simple unit tests

---
+++ ToDoApp/ToDoApp.BLL.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using ToDoApp.BLL.Attributes.Task;
+using ToDoApp.DAL.Enums;
+
+namespace ToDoApp.BLL.Tests;
+
+public class ValidStatusAttributeTests
+{
+    [Fact]
+    public void IsValid_ReturnsTrue_ForKnownStatus()
+    {
+        var attribute = new ValidStatusAttribute();
+
+        var isValid = attribute.IsValid(Status.Done);
+
+        Assert.True(isValid);
+    }
+
+    [Fact]
+    public void IsValid_ReturnsFalse_ForUnknownStatusValue()
+    {
+        var attribute = new ValidStatusAttribute();
+
+        var isValid = attribute.IsValid((Status)999);
+
+        Assert.False(isValid);
+    }
+}
